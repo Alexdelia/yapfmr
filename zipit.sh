ec01461f77c0f892ada74a115885d50dc651ca8a
Alexdelia <alexandre.delille.57@gmail.com>
2024-01-17 19:20:39 +0100

    better `zipit`

--- zipit.sh
+++ zipit.sh
@@ -3,6 +3,12 @@
 find . -maxdepth 1 -name "*.zip" -type f -delete
 
 for folder in */; do
+	# check if `thumbnail.png` exists
+	if [ ! -f "$folder/thumbnail.png" ]; then
+		printf "  \033[1;35m%s\033[31m does not have a \033[1;35mthumbnail.png\033[31m file\033[0m\n" "$folder"
+		continue
+	fi
+
 	version=$(grep -oP '(?<="version": ")[^"]*' "$folder/info.json")
 
 	archive="${folder%/}_$version.zip"
