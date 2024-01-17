34580ee9502768d12172ad272bf83985ab5c09d6
Alexdelia <alexandre.delille.57@gmail.com>
2024-01-17 22:49:49 +0100

    save

--- no-more-oil/prototypes/recipe.lua
+++ no-more-oil/prototypes/recipe.lua
@@ -40,7 +40,7 @@ data:extend({
 				type = "item",
 				name = "salt",
 				amount = 4
-			}
+			},
 			{
 				type = "item",
 				name = "sulfur",
@@ -60,12 +60,11 @@ data:extend({
 		order = "b[fluid-chemistry]-b[plastic]"
 	},
 	{
-	    type = "recipe",
-	    name = "solid-fuel-from-sulfur-and-coal",
-	    category = "chemistry",
-	    energy_required = 2,
-	    ingredients =
-	    {
+		type = "recipe",
+		name = "solid-fuel-from-sulfur-and-coal",
+		category = "chemistry",
+		energy_required = 2,
+		ingredients = {
 			{
 				type = "item",
 				name = "coal",
@@ -76,30 +75,73 @@ data:extend({
 				name = "sulfur",
 				amount = 1
 			}
-	    },
-	    results =
-	    {
+		},
+		results = {
 			{
-				type="item",
-				name="solid-fuel",
-				amount=1
+				type = "item",
+				name = "solid-fuel",
+				amount = 1
 			}
-	    },
-	    icon = "__no-more-oil__/graphics/recipe/solid-fuel.png",
-	    icon_size = 64,
-	    subgroup = "fluid-recipes",
-	    enabled = false,
-	    order = "b[fluid-chemistry]-f[solid-fuel-from-sulfur-and-coal]",
-	    crafting_machine_tint =
-	    {
-			-- 226, 214, 144
-	    	primary = {r = 0.886, g = 0.839, b = 0.565, a = 1.000}, -- #e2d690ff
-			-- 171, 172, 150
-	    	secondary = {r = 0.671, g = 0.675, b = 0.588, a = 1.000}, -- #abac96ff
-		  	-- 181, 197, 160
-	    	tertiary = {r = 0.710, g = 0.773, b = 0.627, a = 1.000}, -- #b5c5a0ff
-			-- 140, 143, 92
-	    	quaternary = {r = 0.549, g = 0.561, b = 0.361, a = 1.000}, -- #8c8f5cff
-	    }
+		},
+		icon = "__no-more-oil__/graphics/recipe/solid-fuel.png",
+		icon_size = 64,
+		subgroup = "fluid-recipes",
+		enabled = false,
+		order = "b[fluid-chemistry]-f[solid-fuel-from-sulfur-and-coal]",
+		crafting_machine_tint = {
+			primary = {
+				r = 0.886,
+				g = 0.839,
+				b = 0.565,
+				a = 1
+			},
+			secondary = {
+				r = 0.671,
+				g = 0.675,
+				b = 0.588,
+				a = 1
+			},
+			tertiary = {
+				r = 0.71,
+				g = 0.773,
+				b = 0.627,
+				a = 1
+			},
+			quaternary = {
+				r = 0.549,
+				g = 0.561,
+				b = 0.361,
+				a = 1
+			}
+		}
+	},
+	{
+		type = "recipe",
+		name = "rocket-fuel-without-oil",
+		category = "crafting",
+		enabled = false,
+		energy_required = 20,
+		ingredients = {
+			{
+				type = "item",
+				name = "iron-plate",
+				amount = 1
+			},
+			{
+				type = "item",
+				name = "sulfur",
+				amount = 2
+			},
+			{
+				type = "item",
+				name = "salt",
+				amount = 1
+			}
+		},
+		results = "rocket-fuel",
+		subgroup = "fluid-recipes",
+		icon = "__no-more-oil__/graphics/recipe/plastic.png",
+		icon_size = 64,
+		order = "b[fluid-chemistry]-b[plastic]"
 	}
 });
