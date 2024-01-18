c544cbd38591fa17aedcefbb2f2db2de46a3ee6b
Alexdelia <alexandre.delille.57@gmail.com>
2024-01-18 21:26:56 +0100

    fix

--- salt/prototypes/item.lua
+++ salt/prototypes/item.lua
@@ -1,9 +1,11 @@
 data:extend({
-	type = "item",
-	name = "salt",
-	icon = "__salt__/graphics/icons/salt.png",
-	icon_size = 64,
-	subgroup = "raw-material",
-	order = "h[salt]",
-	stack_size = 100
+	{
+		type = "item",
+		name = "salt",
+		icon = "__salt__/graphics/icons/salt.png",
+		icon_size = 64,
+		subgroup = "raw-material",
+		order = "h[salt]",
+		stack_size = 100
+	}
 });
