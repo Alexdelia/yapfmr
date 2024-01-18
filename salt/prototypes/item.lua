dc2028855dc05116b52c995dcf36d3a8a7ae58fd
Alexdelia <alexandre.delille.57@gmail.com>
2024-01-18 21:21:59 +0100

    fix

---
+++ salt/prototypes/item.lua
@@ -0,0 +1,9 @@
+data:extend({
+	type = "item",
+	name = "salt",
+	icon = "__salt__/graphics/icons/salt.png",
+	icon_size = 64,
+	subgroup = "raw-material",
+	order = "h[salt]",
+	stack_size = 100
+});
